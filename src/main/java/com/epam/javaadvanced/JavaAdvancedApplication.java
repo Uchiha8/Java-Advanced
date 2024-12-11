9963c4aca0780b07682febc7dec3c466689c1a8c
[200~2110107@newuu.uz <2110107@newuu.uz>
2024-12-11 15:54:05 +0500

    initial

--- src/main/java/com/epam/javaadvanced/JavaAdvancedApplication.java
+++ src/main/java/com/epam/javaadvanced/JavaAdvancedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaAdvancedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaAdvancedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdvancedApplication.class, args);
+    }
 
 }
--- src/test/java/com/epam/javaadvanced/JavaAdvancedApplicationTests.java
+++ src/test/java/com/epam/javaadvanced/JavaAdvancedApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaAdvancedApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
